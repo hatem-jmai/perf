567ecce4e3a42766d68716e8ac941eb4eb34750e
Bouzezi <57668229+Bouzezi@users.noreply.github.com>
2020-06-29 23:06:06 +0100

    login et update coach update client

--- src/app/updatecoach/updatecoach.component.ts
+++ src/app/updatecoach/updatecoach.component.ts
@@ -17,13 +17,38 @@ export class UpdatecoachComponent implements OnInit {
     this.user=new coach();
     console.log(this.authservice.username);
     this.username=this.authservice.username;
+    this.getOneCoach();
   }
-  update(){
-    console.log(this.user);
-    console.log(this.username);
-  this.authservice.updatecoach(this.username,this.user).subscribe((data:any) => {
-    console.log(data);
-    this.username=data;
-});
-}
+        update(){
+          console.log(this.user);
+          console.log(this.username);
+        this.authservice.updatecoach(this.username,this.user).subscribe((data:any) => {
+          console.log(data);
+          this.username=data;
+      });
+      }
+      getOneCoach(){
+        console.log(this.username);
+        this.authservice.getOneCoach(this.username).subscribe((data:any) => {
+        console.log(data);
+        //this.user=data;
+        for (let key in data)
+        if(data.hasOwnProperty(key)){
+          this.user.id=data[key].id;
+          this.user.nom=data[key].nom;
+          this.user.prenom=data[key].prenom;
+          this.user.username=data[key].username;
+          this.user.password=data[key].password;
+          this.user.genre=data[key].genre;
+          this.user.date_naiss=data[key].date_naiss;
+          this.user.cin=data[key].cin;
+          this.user.adresse_mail=data[key].adresse_mail;
+          this.user.tel=data[key].tel;
+          this.user.type=data[key].type;
+          this.user.ville=data[key].ville;
+          this.user.certificat=data[key].certificat;
+        }
+          
+      });
+    }
 }
